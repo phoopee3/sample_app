36f0e12ba13fa3db60e1398059576683ae270adc
Jason Lawton <lawton.jason@gmail.com>
2011-04-28 07:40:27 -0400

    Removed default Rails page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,16 +2,20 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+  
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
 
-  describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
+  describe "GET 'about'" do
+    it "shoud be successful" do
+      get 'about'
       response.should be_success
     end
-    
+
     it "should have the correct title" do
-      get 'home'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
+      get 'about'
+      response.should have_selector("title", :content => @base_title + " | About")
     end
   end
 
@@ -23,19 +27,31 @@ describe PagesController do
 
     it "should have the correct title" do
       get 'contact'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title", :content => @base_title + " | Contact")
     end
   end
-
-  describe "GET 'about'" do
-    it "shoud be successful" do
-      get 'about'
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
       response.should be_success
     end
 
     it "should have the correct title" do
-      get 'about'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About")
+      get 'help'
+      response.should have_selector("title", :content => @base_title + " | Help")
+    end
+  end
+  
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+    
+    it "should have the correct title" do
+      get 'home'
+      response.should have_selector("title", :content => @base_title + " | Home")
     end
   end
 
